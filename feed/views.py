f23ce95940e216bb32175e8da2b8dc968fa4c430
Krish Goel <krishgoel3@gmail.com>
2023-10-27 13:14:36 +0530

    Icba

--- feed/views.py
+++ feed/views.py
@@ -6,7 +6,6 @@ import random
 import time
 
 def load_openai_api_key():
-	# api_key = os.getenv("OPENAI_API_KEY")
 	api_key = "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"
 	if api_key is None:
 		raise EnvironmentError("OpenAI API key not found in .env file")
@@ -38,7 +37,7 @@ def call_openai_api(api_key, user_message):
 	try:
 		start_time = time.time()
 		response = requests.post(api_url, headers=headers, json=request_data, timeout=1000)
-		response.raise_for_status()  # Raise an exception for non-2xx response codes
+		response.raise_for_status()
 
 		if time.time() - start_time > 10:
 			print("It is taking too much time")
@@ -66,19 +65,81 @@ def handle_openai_response(response):
 		print(response.text)
 		return f"Request failed with status code: {response.status_code}"
 
+from google.cloud import texttospeech
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'google_config.json'
+
+def generate_audio(transcript):
+	print(f"Generating audio for {transcript}")
+	try:
+		client = texttospeech.TextToSpeechClient()
+
+		voice = texttospeech.VoiceSelectionParams(
+			language_code="en-US",
+			name="en-US-Studio-M"
+		)
+
+		audio_config = texttospeech.AudioConfig(
+			audio_encoding=texttospeech.AudioEncoding.LINEAR16,
+			pitch=-4,
+			speaking_rate=1.15
+		)
+
+		synthesis_input = texttospeech.SynthesisInput(text=transcript)
+
+		output_directory = "static/content"
+		if not os.path.exists(output_directory):
+			os.makedirs(output_directory)
+
+		output_path = os.path.join(output_directory, "speech.mp3")
+
+		if os.path.exists(output_path):
+			os.remove(output_path)
+
+		response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
+
+		with open(output_path, "wb") as out_file:
+			out_file.write(response.audio_content)
+			print(f"Audio content written to '{output_path}' file.")
+		
+		return output_path.replace("static/", "")
+	
+	except Exception as e:
+		print(f"Audio generation failed: {e}")
+		return None
+
+from moviepy.editor import VideoFileClip, AudioFileClip
+
+def generate_video(audio_path):
+	try:
+		random_number = random.randint(1, 1)
+		video_path = VideoFileClip(f"static/videos/{random_number}.mp4")
+		audio_clip = AudioFileClip(audio_path)
+
+		final_video = video_path.set_audio(audio_clip)
+		final_video.write_videofile("static/output_video.mp4", codec="libx264", audio_codec="aac")
+
+		print("Video generated successfully, saved at static/output_video.mp4")
+		return "output_video.mp4"
+	except Exception as e:
+		print(f"Video generation failed: {e}")
+		return None
+
 def core_render(request):
 	if request.method == 'POST':
 		form = UserInputForm(request.POST)
 		if form.is_valid():
 			topic = form.cleaned_data['topic']
-			prompt = get_random_user_message(topic)
+			prompt_generated = get_random_user_message(topic)
 			openai_api_key = load_openai_api_key()
-			response = call_openai_api(openai_api_key, prompt)
-			content = handle_openai_response(response)
-			return render(request, 'feed/feed.html', {'topic': topic, 'prompt': prompt, 'response': content})
+			response = call_openai_api(openai_api_key, prompt_generated)
+			transcript = handle_openai_response(response)
+			audio = generate_audio(transcript)
+			video = generate_video(audio)
+			return render(request, 'feed/feed.html', {'topic': topic, 'prompt': prompt_generated, 'response': transcript, 'audio': audio, 'video': video})
 	else:
 		form = UserInputForm()
 	return render(request, 'feed/index.html', {'form': form})
 
 def feed_render(request):
-	return render(request, 'feed/feed.html', {'topic': 'undefined', 'prompt': 'undefined', 'response': 'undefined'})
+	return render(request, 'feed/feed.html', {'topic': 'undefined', 'prompt': 'undefined', 'response': 'undefined', 'audio': None, 'video': None})
