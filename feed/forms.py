2b000b37c7d036a9df26cecbbe39cac21ffcb4e8
Krish Goel <krishgoel3@gmail.com>
2023-10-27 10:52:13 +0530

    Fixed the UI

--- feed/forms.py
+++ feed/forms.py
@@ -7,7 +7,7 @@ class UserInputForm(forms.ModelForm):
 	"""
 	topic = forms.CharField(
 		label="topic",
-		widget=forms.TextInput(attrs={"class": "block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"}),
+		widget=forms.TextInput(attrs={"class": "block w-full rounded-md border-0 py-1.5 pl-4 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6", "placeholder": "Today I'm curious about"}),
 	)
 
 	class Meta:
